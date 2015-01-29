f8f04525b21869a434aa69e680438f6ce39668ae
sxcer <sxcerr@gmail.com>
2015-01-29 07:20:10 -0500

    add missing function end brace

--- sxc_winbuild.sh
+++ sxc_winbuild.sh
@@ -209,7 +209,7 @@ OPENSSL_MD5=cdb22925fc9bc97ccbf1e007661f2aa6
 OPENSSL_MSYS2_BUILDCMDS="# openssl build commands
 ./Configure no-zlib no-shared no-dso no-krb5 no-camellia no-capieng no-cast no-cms no-dtls1 no-gost no-gmp no-heartbeats no-idea no-jpake no-md2 no-mdc2 no-rc5 no-rdrand no-rfc3779 no-rsax no-sctp no-seed no-sha0 no-static_engine no-whirlpool no-rc2 no-rc4 no-ssl2 no-ssl3 mingw64
 make"
-
+}
 
 function init_BDB_vars() {
 BDB=db
